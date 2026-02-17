2006fa7dd1ab51958dc48b2cd979798362ba4305
drodrigor <danierrodrigo@gmail.com>
2026-02-17 13:02:45 +0100

    operadores aritméticos

---
+++ Curso Python/UD 2 - Operaciones y Expresiones/operadoresAritméticos.py
@@ -0,0 +1,25 @@
+# Ejemplo de operadores aritméticos en Python
+
+a = 10
+b = 3
+
+# Suma
+print("Suma:", a + b)
+
+# Resta
+print("Resta:", a - b)
+
+# Multiplicación
+print("Multiplicación:", a * b)
+
+# División
+print("División:", a / b)
+
+# División entera
+print("División entera:", a // b)
+
+# Módulo (resto)
+print("Módulo:", a % b)
+
+# Potencia
+print("Potencia:", a ** b)
