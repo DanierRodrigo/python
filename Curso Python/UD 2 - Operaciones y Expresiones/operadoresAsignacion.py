e03f3ee13c3868037d6325e0689fab20b9413166
drodrigor <danierrodrigo@gmail.com>
2026-02-17 13:37:17 +0100

    operadores de asignacion

---
+++ Curso Python/UD 2 - Operaciones y Expresiones/operadoresAsignacion.py
@@ -0,0 +1,31 @@
+x = 10   # Asignación básica
+
+print("Valor inicial:", x)
+
+# Suma y asigna
+x += 5
+print("Después de x += 5:", x)
+
+# Resta y asigna
+x -= 3
+print("Después de x -= 3:", x)
+
+# Multiplica y asigna
+x *= 2
+print("Después de x *= 2:", x)
+
+# Divide y asigna
+x /= 4
+print("Después de x /= 4:", x)
+
+# División entera y asigna
+x //= 2
+print("Después de x //= 2:", x)
+
+# Módulo y asigna
+x %= 3
+print("Después de x %= 3:", x)
+
+# Potencia y asigna
+x **= 2
+print("Después de x **= 2:", x)
