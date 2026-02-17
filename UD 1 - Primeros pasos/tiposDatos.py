5f01803238887ecd8ab4ef5ceb4a9811bd40e554
drodrigor <danierrodrigo@gmail.com>
2026-02-17 12:48:12 +0100

    tipos de datos

---
+++ UD 1 - Primeros pasos/tiposDatos.py
@@ -0,0 +1,13 @@
+#Datos básicos en Python
+
+flotante=15.5
+print(flotante)
+print(type(flotante))
+
+booleano=True
+print(booleano)
+print(type(booleano))
+
+ninguno=None
+print(ninguno)
+print(type(ninguno))
