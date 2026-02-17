51257b9cba1c44f197932128d4f4848a6f5643a8
drodrigor <danierrodrigo@gmail.com>
2026-02-17 13:35:20 +0100

    operadores relacionales

---
+++ Curso Python/UD 2 - Operaciones y Expresiones/operadoresRelacionales.py
@@ -0,0 +1,22 @@
+# Ejemplo de operadores relacionales en Python
+
+a = 10
+b = 5
+
+# Igual que
+print("¿a es igual a b?:", a == b)
+
+# Diferente de
+print("¿a es diferente de b?:", a != b)
+
+# Mayor que
+print("¿a es mayor que b?:", a > b)
+
+# Menor que
+print("¿a es menor que b?:", a < b)
+
+# Mayor o igual que
+print("¿a es mayor o igual que b?:", a >= b)
+
+# Menor o igual que
+print("¿a es menor o igual que b?:", a <= b)
