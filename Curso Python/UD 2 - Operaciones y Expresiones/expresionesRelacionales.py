6efcc040e47e7cfa47a0fc6ee31ecac84f9b16ba
drodrigor <danierrodrigo@gmail.com>
2026-02-17 13:42:31 +0100

    expresiones relacionales

---
+++ Curso Python/UD 2 - Operaciones y Expresiones/expresionesRelacionales.py
@@ -0,0 +1,7 @@
+nota1 = 7.5
+nota2 = 6.0
+
+print("¿Las notas son iguales?:", nota1 == nota2)
+print("¿La primera nota es mayor?:", nota1 > nota2)
+print("¿La segunda nota es menor o igual a 5?:", nota2 <= 5)
+print("¿Las notas son diferentes?:", nota1 != nota2)
