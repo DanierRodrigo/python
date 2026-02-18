3d8194f8a1472b1b9432b0c0f6205081803a128a
drodrigor <danierrodrigo@gmail.com>
2026-02-18 23:41:49 +0100

    sentencias de control if

---
+++ Curso Python/UD 3 - Sentencias Control de Flujo/sentenciaIf.py
@@ -0,0 +1,8 @@
+temperatura = 18
+
+if temperatura < 10:     #Primera condición
+    print("Hace frío")
+elif temperatura <= 25:  #Segunda condición además de la primera
+    print("Temperatura agradable")
+else:                    #En caso de que no se cumpla ninguna
+    print("Hace calor")
