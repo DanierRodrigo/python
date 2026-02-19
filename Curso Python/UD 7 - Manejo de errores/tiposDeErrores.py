91c49c904103595f0b1930d515bfd81e00f22aaf
drodrigor <danierrodrigo@gmail.com>
2026-02-19 13:24:31 +0100

    tipos de errores

---
+++ Curso Python/UD 7 - Manejo de errores/tiposDeErrores.py
@@ -0,0 +1,18 @@
+# =====================================================
+# TIPOS DE ERRORES
+# =====================================================
+
+# Error de sintaxis
+
+if True
+    print("Hola")
+
+# Error en tiempo de ejecución
+
+print(10 / 0)
+
+# Errores lógicos
+
+def suma(a, b):
+    return a - b  # Error lógico
+
